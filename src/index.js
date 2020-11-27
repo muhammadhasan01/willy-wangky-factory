57edbbe4a7a7be87f7cbb6632d3c7c19b6d93da3
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2020-11-27 16:02:53 +0700

    Add login component from login branch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './pages/_app';
-
 import './styles/main.bundle.css';
 
 ReactDOM.render(
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { Component } from "react";
+import { Button, Form } from "react-bootstrap";
+import './login.css';
 
-import {DashboardLayout} from '../components/Layout';
+export default class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
 
-const Login = () => {
-  return (
-    <DashboardLayout>
-      <h2>Ceritanya ini login</h2>
-    </DashboardLayout>
-  )
+    render() {
+        return (
+            <div class="Login">
+                <div class="Login-container">
+                    <div class="title">Willy Wangky Factory</div>
+                    <div class='text-muted text-center'>
+                    Login terlebih dahulu untuk menggunakan factory ini!
+                    </div>
+                    <Form>
+                        <Form.Group size="lg" controlId="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control
+                            autoFocus
+                            type="email"
+                            value={this.email}
+                            onChange={(e) => this.setState({email : e.target.value})}
+                        />
+                        </Form.Group>
+                        <Form.Group size="lg" controlId="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            value={this.password}
+                            onChange={(e) => this.setState({password : e.target.value})}
+                        />
+                        </Form.Group>
+                        <Button block size="lg" type="submit" disabled={false}>
+                            Login
+                        </Button>
+                    </Form>
+                </div>
+            </div>
+        );
+    }
 }
-
-export default Login;
